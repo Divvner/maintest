0f59bc2d14d0cd163581fc33118604e963d6f3f5
Sergei Pavlov <104250634+Divvner@users.noreply.github.com>
2022-06-07 13:19:21 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,84 @@
+// Loader
+
+function preloader() {
+	$(() => {
+
+		setInterval(() => {
+
+			let preloader = $('#preloader');
+
+			preloader.css('opacity', 0);
+			preloader.css('transition', 0.4);
+			preloader.css('visibility', 'hidden');
+			preloader.css('visibility', 0, 1);
+
+		}, 1300);
+
+	});
+}
+
+preloader();
+
+// 3D Scroll
+
+let zSpacing = -1000,
+		lastPos = zSpacing / 2.4,
+		$frames = document.getElementsByClassName('frame'),
+		frames = Array.from($frames),
+		zVals = []
+
+window.onscroll = function() {
+
+	let top = document.documentElement.scrollTop,
+			delta = lastPos - top
+
+	lastPos = top
+
+	frames.forEach(function(n, i) {
+		zVals.push((i * zSpacing) + zSpacing)
+		zVals[i] += delta * -2.9
+		let frame = frames[i],
+				transform = `translateZ(${zVals[i]}px)`,
+				opacity = zVals[i] < Math.abs(zSpacing) / 1.8 ? 1 : 0
+		frame.setAttribute('style', `transform: ${transform}; opacity: ${opacity}`)
+	})
+
+}
+
+window.scrollTo(0, 1)
+
+// Audio
+
+let soundButton = document.querySelector('.soundbutton'),
+		audio = document.querySelector('.audio')
+
+soundButton.addEventListener('click', e => {
+	soundButton.classList.toggle('paused')
+	audio.paused ? audio.play() : audio.pause()
+})
+
+window.onfocus = function() {
+	soundButton.classList.contains('paused') ? audio.pause() : audio.play()
+}
+
+
+/*
+
+Главная(#), Music, Авторы, Labels, Flyte
+
+*/
+
+/*
+
+	include JQuery
+
+*/
+
+$('.mobile div').on('click', () => {
+    $('.mobile div').toggleClass('active');
+    $('.mobile nav').toggleClass('open');
+    $('.mobile nav ul').toggleClass('show');
+});
+for (let a = 1; a <= $(".mobile ul li").length; a++){ 
+    $(".mobile ul li:nth-child("+ a +")").css("animation-delay", "."+ (a+1) +"s");    
+}
